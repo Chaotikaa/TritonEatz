10689e718723a28ad4918d37df0d3e0aa1cc65d6
Ben Llanes <chtka.ow@gmail.com>
2017-05-22 22:26:49 -0700

    Finished web scraping functionality

--- server.js
+++ server.js
@@ -1,28 +1,9 @@
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
+var URLS = require('./utils/url.js');
 
 var app = express();
-
-const URL_CAFE_VENTANAS = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=18';
-const URL_CANYON_VISTA = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=24;';
-const URL_64_DEGREES = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=64';
-const URL_FOODWORX = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=11';
-const URL_OCEANVIEW = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=05';
-const URL_PINES = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=01';
-const URL_BISTRO = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=27';
-const URL_CLUB_MED = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=15';
-const URL_FLAVORS_OF_THE_WORLD = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=39';
-const URL_GOODYS = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=06';
-const URL_GOODYS_TO_GO = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=38';
-const URL_ROOTS = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=38';
-const URL_64_NORTH = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=38';
-
-
-const URLS = [URL_CAFE_VENTANAS, URL_CANYON_VISTA, URL_64_DEGREES, 
-  URL_FOODWORX, URL_OCEANVIEW, URL_PINES, URL_BISTRO, URL_CLUB_MED,
-  URL_FLAVORS_OF_THE_WORLD, URL_GOODYS, URL_GOODYS_TO_GO, URL_ROOTS, 
-  URL_64_NORTH];
 
 app.use(express.static('public'));
 
@@ -66,8 +47,6 @@ app.get('/scrape', (req, res) => {
 
         $(elm).children().each((i, e) => {
 
-          // console.log(e.tagName);
-
           if (e.tagName === 'p') {
             curr_sub = $(e).text();
           }
@@ -92,10 +71,14 @@ app.get('/scrape', (req, res) => {
               var price = text.slice(price_loc);
               var category = curr_cat;
               var subcategory = curr_sub;
+
               price = price.substr(1, price.length - 2);
 
               var item = {
-                name, price, category, subcategory
+                name, 
+                price, 
+                category, 
+                subcategory
               };
 
               if (name != '' && item != ''){
@@ -103,7 +86,6 @@ app.get('/scrape', (req, res) => {
               }
             }
           }
-
         });
       });
 
---
+++ utils/url.js
@@ -0,0 +1,21 @@
+const URL_CAFE_VENTANAS = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=18';
+const URL_CANYON_VISTA = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=24;';
+const URL_64_DEGREES = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=64';
+const URL_FOODWORX = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=11';
+const URL_OCEANVIEW = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=05';
+const URL_PINES = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=01';
+const URL_BISTRO = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=27';
+const URL_CLUB_MED = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=15';
+const URL_FLAVORS_OF_THE_WORLD = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=39';
+const URL_GOODYS = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=06';
+const URL_GOODYS_TO_GO = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=38';
+const URL_ROOTS = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=38';
+const URL_64_NORTH = 'https://hdh.ucsd.edu/DiningMenus/default.aspx?i=38';
+
+
+const URLS = [URL_CAFE_VENTANAS, URL_CANYON_VISTA, URL_64_DEGREES, 
+  URL_FOODWORX, URL_OCEANVIEW, URL_PINES, URL_BISTRO, URL_CLUB_MED,
+  URL_FLAVORS_OF_THE_WORLD, URL_GOODYS, URL_GOODYS_TO_GO, URL_ROOTS, 
+  URL_64_NORTH];
+
+module.exports = URLS;
